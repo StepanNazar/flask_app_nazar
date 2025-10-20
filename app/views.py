9a0a2091b097fe8cc89127e7d47809acadc0bdfe
Nazar Stepan <64284448+StepanNazar@users.noreply.github.com>
2025-10-20 16:28:25 +0300

    feat: add products and user greetings

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import request, redirect, url_for, render_template, abort
 from . import app
+from .models import product_repo
 
 @app.route("/")
 @app.route("/resume")
@@ -10,3 +11,29 @@ def resume() -> str:
 @app.route("/contact")
 def contact() -> str:
     return render_template("contact.html", title="Contact")
+
+@app.route('/hi/')
+@app.route("/hi/<string:name>")
+def greetings(name: str = "Guest") -> str:
+    name = name.upper()
+    age = request.args.get("age", None, int)
+    return render_template("hi.html", name=name, age=age)
+@app.route("/admin")
+def admin():
+    return redirect(
+        url_for("greetings", name="administrator", age=45, _external=True)
+    )
+
+@app.route('/products')
+def products():
+    products = product_repo.get_all()
+    return render_template("products.html",
+                           products=products)
+
+@app.route('/product/<int:id>')
+def detail_post(id):
+    if id > 3:
+        abort(404)
+    product = product_repo.get_by_id(id)
+    return render_template("product_details.html",
+                           product=product)
