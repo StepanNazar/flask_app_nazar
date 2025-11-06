ef0ebdfbe1b275adc1ec72741c56733b32cb9317
Nazar Stepan <64284448+StepanNazar@users.noreply.github.com>
2025-11-07 01:30:39 +0200

    feat: add custom 404 error page and add create_app factory

--- app/views.py
+++ app/views.py
@@ -1,7 +1,9 @@
-from flask import redirect, url_for
-from . import app
+from flask import redirect, url_for, Blueprint
 
 
-@app.route("/")
+root_bp = Blueprint(
+    "root path", __name__, template_folder="templates", static_folder="static"
+)
+@root_bp.route("/")
 def resume():
     return redirect(url_for("resume.resume"))
